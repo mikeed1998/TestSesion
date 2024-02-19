8d42def1ddeeea798b909fe5ffc999fadb01f279
Michael <mikeed1998@gmail.com>
2024-02-18 23:46:24 -0600

    sesiones

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Support\Facades\Auth;
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if(Auth::check())
+        {
+            if(Auth::user()->role_as == 1)
+            {
+                return $next($request);
+            }
+            else 
+            {
+                return redirect('/home')->with('status', 'Access Denied! as you are not as admin');
+            }
+        }
+        else
+        {
+            return redirect('/home')->with('status', 'Please Login First');
+        }
+    }
+}
